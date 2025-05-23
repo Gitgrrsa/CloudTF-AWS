6184f806b613fd22bca6c4402975333840f20faa
Gitgrrsa <64684191+Gitgrrsa@users.noreply.github.com>
2025-05-23 16:34:08 +0200

    Update provider.tf

--- assessment/terraform/provider.tf
+++ assessment/terraform/provider.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+terraform {
+  backend "s3" {
+    bucket = "your-tfstate-bucket"
+    key    = "defensepoint/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
